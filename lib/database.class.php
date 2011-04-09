10f524e07e02b3b9b96e4d01d6a36c322826fbb0
Matthew Perry <perrym5@rpi.edu>
2011-04-09 19:21:41 -0400

    Changed database class to reflect last commit

--- lib/database.class.php
+++ lib/database.class.php
@@ -9,7 +9,6 @@
  *  Class abstraction for mysql database
  *  queries.
  */
-require_once( dirname( __FILE__ ) . '/../config.inc.php' );
 
 class database
 {
@@ -18,18 +17,14 @@ class database
 
     function database()
     {
-        $db_info = new stdClass();
-        $db_info->db_host = DB_HOST;
-        $db_info->db_user = DB_USER;
-        $db_info->db_pass = DB_PASS;
-        $db_info->db_name = DB_NAME;
+        $db_info = parse_ini_file( dirname(__FILE__) . '/../config.ini' );
     }
 
     function connect()
     {
-        if( !($db = mysql_connect( $db_info->db_host, $db_info->db_user, $db_info->db_pass )) )
+        if( !($db = mysql_connect( $db_info[ "host" ], $db_info[ "user" ], $db_info[ "pass" ] )) )
             die( "FATAL: Could not connect to DB server: " . mysql_error() );
-        if( !mysql_select_db( $db_info->db_name, $db ) )
+        if( !mysql_select_db( $db_info[ "name" ], $db ) )
             die( "FATAL: Could not connect to DB: " . mysql_error() );
     }
 
