442980c3b07a4a05f2a16ac9da99bed76bf10907
Matthew Perry <perrym5@rpi.edu>
2011-04-09 19:07:25 -0400

    Added place holder index and moved some stuff around

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Recommender Service for Morning Mail
+ *
+ * FILE: index.php
+ * COURSE: Web Systems Development
+ * AUTHOR(s): Matthew Perry
+ * DESCRIPTION:
+ *  Main page, shows relevant links based on QT
+ *  clustering algorithm
+ */
+
+echo "<h1>WORK IN PROGRESS</h1>";
+?>
