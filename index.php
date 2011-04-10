282cbf190a12213c5a1192cddae27b2f56c76828
Matthew Perry <perrym5@rpi.edu>
2011-04-09 22:30:37 -0400

    Added some placeholder code to index.php just to show one part of my new API

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
  *  Main page, shows relevant links based on QT
  *  clustering algorithm
  */
+require_once( "lib/user.class.php" );
 
 echo "<h1>WORK IN PROGRESS</h1>";
+echo "<h2>All User IDs:</h2>";
+echo "<ul>";
+foreach( User::getUIDs() as $uid )
+    echo "<li>".$uid."</li>";
+echo "</ul>";
 ?>
